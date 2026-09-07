6d8b1ffb65612751deca3c6f95551656a71f1ecd
alejandrovallecillos <alejandrovallecillos@gmail.com>
2026-09-07 19:48:07 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE = 'graccus-0-12-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './graccus-0.12.html',
+  './manifest.json',
+  './iconos/icon-180.png',
+  './iconos/icon-192.png',
+  './iconos/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        if (response && response.ok && new URL(event.request.url).origin === self.location.origin) {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
